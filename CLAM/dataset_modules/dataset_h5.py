d48b228a74dd0edc7394dcb6a6314881576192f8
RDLigeralde <rdligeralde@gmail.com>
2024-07-08 18:14:40 -0400

    feat: add CLAM contents

---
+++ CLAM/dataset_modules/dataset_h5.py
@@ -0,0 +1,107 @@
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+from mpi4py import MPI
+import torch
+
+import numpy as np
+import pandas as pd
+import os
+
+from CLAM.wsi_core.WholeSlideImage import WholeSlideImage
+import openslide
+import h5py
+
+from typing import Optional
+from collections.abc import Iterable
+
+class Whole_Slide_Bag(Dataset):
+	def __init__(self,
+		file_path,
+		img_transforms=None):
+		"""
+		Args:
+			file_path (string): Path to the .h5 file containing patched data.
+			roi_transforms (callable, optional): Optional transform to be applied on a sample
+		"""
+		self.roi_transforms = img_transforms
+		self.file_path = file_path
+
+		with h5py.File(self.file_path, "r") as f:
+			dset = f['imgs']
+			self.length = len(dset)
+			
+	def __len__(self):
+		return self.length
+
+	def summary(self):
+		with h5py.File(self.file_path, "r") as hdf5_file:
+			dset = hdf5_file['imgs']
+			for name, value in dset.attrs.items():
+				print(name, value)
+
+		print('transformations:', self.roi_transforms)
+
+	def __getitem__(self, idx):
+		with h5py.File(self.file_path,'r') as hdf5_file:
+			img = hdf5_file['imgs'][idx]
+			coord = hdf5_file['coords'][idx]
+		
+		img = Image.fromarray(img)
+		img = self.roi_transforms(img)
+		return {'img': img, 'coord': coord}
+
+class Whole_Slide_Bag_FP(Dataset):
+	def __init__(self,
+		file_path: str,
+		wsi_path: str,
+		img_transforms=None):
+		"""
+		Dataset of patches from single WSi
+
+		Args:
+			file_path (str): path to the .h5 file containing patched data
+			wsi (str): path to target WSI
+			img_transforms (callable, optional): patch transforms
+		"""
+		self.wsi = openslide.OpenSlide(wsi_path)
+		self.roi_transforms = img_transforms
+
+		self.file_path = file_path
+
+		with h5py.File(self.file_path, "r") as f:
+			dset = f['coords']
+			self.patch_level = f['coords'].attrs['patch_level']
+			self.patch_size = f['coords'].attrs['patch_size']
+			self.length = len(dset)
+			
+	def __len__(self):
+		return self.length
+
+	def summary(self):
+		hdf5_file = h5py.File(self.file_path, "r")
+		dset = hdf5_file['coords']
+		for name, value in dset.attrs.items():
+			print(name, value)
+
+		print('\nfeature extraction settings')
+		print('transformations: ', self.roi_transforms)
+
+	def __getitem__(self, idx):
+		with h5py.File(self.file_path,'r') as hdf5_file:
+			coord = hdf5_file['coords'][idx]
+		img = self.wsi.read_region(coord, self.patch_level, (self.patch_size, self.patch_size)).convert('RGB')
+		if self.roi_transforms is not None:
+			img = self.roi_transforms(img)
+		return img, coord
+	
+class Dataset_All_Bags(Dataset):
+
+	def __init__(self, csv_path):
+		self.df = pd.read_csv(csv_path)
+	
+	def __len__(self):
+		return len(self.df)
+
+	def __getitem__(self, idx):
+		return self.df['slide_id'][idx]
